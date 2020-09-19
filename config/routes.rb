6d49d78f6613dd7e3b6e4816ea7c549048da7f35
sophie rouchon <sophie.rouchon@gmail.com>
2020-09-19 12:44:19 +0200

    problem solving on routes + implement products#show for beginning

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'categories/show'
   devise_for :users
   root to: 'pages#home'
-  resources :category, only: [:show]
-  resources :product, only: [:show] do
-    resources :rating, only: [:new, :create]
+  resources :categories, only: [:show]
+  resources :products, only: [:show] do
+    resources :ratings, only: [:new, :create]
   end
-  resources :rating, only: [:destroy]
+  resources :ratings, only: [:destroy]
 end
