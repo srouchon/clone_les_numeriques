d95ce94b414010d6b3a3eb864fcd83b7714510af
sophie rouchon <sophie.rouchon@gmail.com>
2020-09-22 08:55:52 +0200

    implement products#edit and update controllers+views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,11 +23,17 @@ class ProductsController < ApplicationController
   end
   
   def edit
-    
+    @product = Product.find(params[:id])
   end
   
   def update
-    
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
   
   def destroy
