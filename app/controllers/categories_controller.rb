bfc273c0d0e8d778071759213c65970174814853
sophie rouchon <sophie.rouchon@gmail.com>
2020-09-22 17:13:03 +0200

    add categories#show + modif on navbar - need to improve frontend in every pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   
   def show
-    
+    @category = Category.find(params[:id])
+    @products = @category.products
   end
 end
